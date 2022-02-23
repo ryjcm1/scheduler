69b7479c0744c514bd240b53003a5fc29dd1b921
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-22 21:44:55 -0500

    add Appointment component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "../InterviewerList"
+
+export default function Form(props){
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return(
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+            value={student}
+            onChange={(event)=> {setStudent(event.target.value)}}
+          />
+        </form>
+        <InterviewerList 
+          /* your code goes here */
+          interviewers = {props.interviewers}
+          interviewer = {interviewer}
+          setInterviewer = {setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger 
+            onClick={() => {
+              props.onCancel()
+              setStudent("")
+              setInterviewer(null)
+              }}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss"
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
+
+
+export default function Appointment(props){
+  // const appointmentStatus = props.time ? `Appointment at ${props.time}` : "No Appointments"
+  const appointmentStatus = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />;
+
+  return(
+    <article className="appointment">
+      <Header time={props.time} />
+      {appointmentStatus}
+    </article>
+  )
+}
