19d61d7b6df96a139611b916e302f5898e4a3a2e
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-23 11:39:51 -0500

    add custom hook useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+export function useVisualMode(initial){
+  const [mode, setMode] = useState({current: initial, previous: null})
+
+  const transition = (newMode) => {
+    const previous = mode.current;
+    setMode({current:newMode, previous: previous})
+  }
+
+  const back = () => {
+    const current = mode.previous;
+    setMode({current:current, previous:null})
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  }
+ 
+
+}
