761d748e8b074e41232324b634c5ecdd3cc24934
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-23 15:50:50 -0500

    add getInterviewersForDay helper function and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -38,3 +38,46 @@ export function getInterview(state, interview) {
   return {...interview, interviewer: interviewerProfile};
 
 }
+
+
+export function getInterviewersForDay(state, day) {
+  if(!state.days || !state.interviewers) return [];
+
+  const days = state.days;
+
+  //holds all appointment IDs for a given day
+  let focusAppointmentId;
+
+  for (let specificDay of days) {
+    if(specificDay.name === day){
+      focusAppointmentId =  specificDay.appointments;
+    }
+  }
+
+  //when day is not found
+  if(!focusAppointmentId) return [];
+
+  //hold all interview IDs for a specific day
+  let interviewerIDs = [];
+
+  const appts = state.appointments;
+
+  for(const appt of Object.values(appts)){
+    if(appt.interview && focusAppointmentId.includes(appt.id)){
+      interviewerIDs.push(appt.interview.interviewer)
+    }
+  }
+
+  //will be populated with interviewer objects
+  let interviewersForDay = [];
+
+  const interviewers = state.interviewers;
+  for(const interviewer of Object.values(interviewers)){
+    if(interviewerIDs.includes(interviewer.id)){
+      interviewersForDay.push(interviewer)
+    }
+  }
+
+  return interviewersForDay;
+
+}
