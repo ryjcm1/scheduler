c8699c3301092f83fcb76914050a2fe90a86af1d
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-28 22:03:14 -0500

    refactored cancel function in appointment edit form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -18,6 +18,17 @@ export default function Form(props){
     props.onSave(name, interviewer);
   }
 
+  function reset(){
+    setName("");
+    setInterviewer(null);
+  }
+
+  function cancel(){
+    reset();
+    props.onCancel();
+  }
+
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -47,11 +58,9 @@ export default function Form(props){
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger 
-            onClick={() => {
-              props.onCancel()
-              setName("")
-              setInterviewer(null)
-              }}>Cancel</Button>
+            onClick={cancel}>
+              Cancel
+          </Button>
           <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
